b2937551a2101ccf1faf635ffbcd900636c541e7
Inna Tarasyan <innatarasyanmail@gmail.com>
2022-11-13 20:00:04 +0400

    Better accessors and mutators in Eloquent

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Blog extends Model
 {
     use HasFactory;
+
+    public function description(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => strtoupper($value),
+                 set: fn ($value) => $value,
+    );
+}
 }
