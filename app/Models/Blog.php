8607e260c0b12d9efc5788a8b567e5623bca3211
Inna Tarasyan <innatarasyanmail@gmail.com>
2022-11-13 20:43:06 +0400

    Fulltext indexes and where clauses

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
+
 class Blog extends Model
 {
     use HasFactory;
@@ -15,6 +16,13 @@ class Blog extends Model
         return new Attribute(
             get: fn ($value) => strtoupper($value),
                  set: fn ($value) => $value,
-    );
-}
+        );
+    }
+
+    public function search($searchText)
+    {
+       return  \DB::table('blog')
+           ->whereFullText('long_description', $searchText)
+           ->get();
+    }
 }
