3a97ebe5f2d0049af89f344c303ef58ff93dd241
Inna Tarasyan <innatarasyanmail@gmail.com>
2022-12-11 21:58:43 +0400

    Listing page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class ListingController extends Controller
 {
@@ -41,5 +45,87 @@ class ListingController extends Controller
             ]);
 
         return redirect()->to($listing->apply_link);
+    }
+
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    public function store(Request $request)
+    {
+        // process the listing creation form
+        $validationArray = [
+            'title' => 'required',
+            'company' => 'required',
+            'logo' => 'required|file|max:2048',
+            'location' => 'required',
+            'apply_link' => 'required|url',
+            'content' => 'required',
+            'payment_method_id' => 'required'
+        ];
+
+        if (!Auth::check()) {
+            $validationArray = array_merge($validationArray, [
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|min:5',
+                'name' => 'required'
+            ]);
+        }
+
+        $request->validate($validationArray);
+
+        $user = Auth::user();
+
+        if (!$user) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $user->createAsStripeCustomer();
+
+            Auth::login($user);
+        }
+
+        try {
+            $amount = 9900;
+            if ($request->filled('is_highlighted')) {
+                $amount += 1900;
+            }
+
+            $user->charge($amount, $request->payment_method_id);
+
+            $md = new \ParsedownExtra();
+
+            $listing = $user->listings()
+                ->create([
+                    'title' => $request->title,
+                    'slug' => Str::slug($request->title) . '-' . rand(1111, 9999),
+                    'company' => $request->company,
+                    'logo' => basename($request->file('logo')->store('public')),
+                    'location' => $request->location,
+                    'apply_link' => $request->apply_link,
+                    'content' => $md->text($request->input('content')),
+                    'is_highlighted' => $request->filled('is_highlighted'),
+                    'is_active' => true
+                ]);
+
+            foreach(explode(',', $request->tags) as $requestTag) {
+                $tag = Tag::firstOrCreate([
+                    'slug' => Str::slug(trim($requestTag))
+                ], [
+                    'name' => ucwords(trim($requestTag))
+                ]);
+
+                $tag->listings()->attach($listing->id);
+            }
+
+            return redirect()->route('dashboard');
+        } catch(\Exception $e) {
+            return redirect()->back()
+                ->withErrors(['error' => $e->getMessage()]);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ use App\Http\Controllers;
 Route::get('/', [Controllers\ListingController::class, 'index'])
     ->name('listings.index');
 
+Route::get('/new', [Controllers\ListingController::class, 'create'])
+    ->name('listings.create');
+
+Route::post('/new', [Controllers\ListingController::class, 'store'])
+    ->name('listings.store');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
