d012b0a503c0cd12e5c1b1f90a2c81b7abb17dab
Inna Tarasyan <innatarasyanmail@gmail.com>
2022-12-18 19:13:19 +0400

    Admin page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,53 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+    <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-12 mx-auto">
+            <div class="mb-12 flex items-center">
+                <h2 class="text-2xl font-medium text-gray-900 title-font px-4">
+                    Your listings ({{ $listings->count() }})
+                </h2>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="ml-3 text-indigo-500">Sign Out</button>
+                </form>
+            </div>
+            <div class="-my-6">
+                @foreach($listings as $listing)
+                    <a href="{{ route('listings.show', $listing->slug) }}"
+                       class="border-gray-900 py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100
+                        {{ $listing->is_highlighted ?
+                        'bg-green-100 hover:bg-green-200' : 'bg-yellow hover:bg-yellow-100' }}">
+                        <div class="md:w-16 md:mb-0 mb-6 mr-4 flex-shrink-0 flex flex-col">
+                            <img src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }} logo"
+                                 class="w-16 h-16 rounded-full object-cover">
+                        </div>
+                        <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
+                            <h2 class="text-xl font-bold text-gray-900 title-font mb-1">{{ $listing->title }}</h2>
+                            <div> {!! strip_tags(substr($listing->content, 0, 200)) !!}  ......</div>
+                            <p class="leading-relaxed text-gray-900">
+                                {{ $listing->company }} &mdash; <span class="text-gray-600">{{ $listing->location }}</span>
+                            </p>
+                        </div>
+                        <div class="md:flex-grow mr-8 flex items-center justify-start">
+                            @foreach($listing->tags as $index => $tag)
+                                @if($index%2 == 0)
+                                    <span class="bg-red-100 text-red-800 text-xs font-semibold
+                                 mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">{{ $tag->name }}</span>
+                                @elseif($index%2 == 1)
+                                    <span class=" bg-green-100 text-green-800 text-xs font-semibold
+                                mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">{{ $tag->name }}</span>
+                                @endif
+                            @endforeach
+                        </div>
+                        <span class="md:flex-grow justify-end">
+                          <span>posted {{ $listing->created_at->diffForHumans() }}</span>
+                          <br/>
+                          <span>
+                              <strong class="text-bold">{{ $listing->clicks()->count() }}</strong> Apply Button Clicks
+                          </span>
+                        </span>
+                    </a>
+                @endforeach
             </div>
         </div>
-    </div>
+    </section>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,10 @@ Route::get('/new', [Controllers\ListingController::class, 'create'])
 Route::post('/new', [Controllers\ListingController::class, 'store'])
     ->name('listings.store');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard', function (\Illuminate\Http\Request $request) {
+    return view('dashboard', [
+        'listings' => $request->user()->listings
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
